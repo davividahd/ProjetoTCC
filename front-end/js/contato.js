8142d3ac3a9846a18360fa54db9751626da621ce
davividahd <daviricardosantos2007@gmail.com>
2025-10-20 18:21:51 -0300

    Update contato.js

--- front-end/js/contato.js
+++ front-end/js/contato.js
@@ -1,32 +1,53 @@
+const btnNovaMensagem = document.getElementById('btnNovaMensagem');
+const modalMensagem = document.getElementById('modalMensagem');
+const btnEnviarMensagem = document.getElementById('btnEnviarMensagem');
+const btnFecharModal = document.getElementById('btnFecharModal');
+const inputNome = document.getElementById('nomeContato');
+const inputTexto = document.getElementById('textoMensagem');
 const contatosDiv = document.querySelector('.contatos');
 
-
 function mostrarMensagensSalvas() {
-    contatosDiv.innerHTML = '';
-    let mensagens = JSON.parse(localStorage.getItem('mensagens')) || [];
-    mensagens.forEach(msg => {
-        const novoContato = document.createElement('div');
-        novoContato.className = 'testecontato';
-        novoContato.innerHTML = `<strong>${msg.nome}:</strong> ${msg.mensagem}`;
-        contatosDiv.appendChild(novoContato);
-    });
+   contatosDiv.innerHTML = '';
+   let mensagens = JSON.parse(localStorage.getItem('mensagens')) || [];
+   mensagens.forEach(msg => {
+      const novoContato = document.createElement('div');
+      novoContato.className = 'testecontato';
+      novoContato.innerHTML = `<strong>${msg.nome}:</strong> ${msg.mensagem}`;
+      contatosDiv.appendChild(novoContato);
+   });
 }
 
 
-mostrarMensagensSalvas();
 
-
-window.addEventListener('storage', function(event) {
-    if (event.key === 'mensagens') {
-        mostrarMensagensSalvas();
-    }
+btnNovaMensagem.addEventListener('click', function () {
+   modalMensagem.style.display = 'block';
 });
 
-const btnDeletar = document.getElementById('btnDeletar');
-
-btnDeletar.addEventListener('click', function() {
- 
-    localStorage.removeItem('mensagens');
-
-    contatosDiv.innerHTML = '';
+btnFecharModal.addEventListener('click', function () {
+   modalMensagem.style.display = 'none';
+   inputNome.value = '';
+   inputTexto.value = '';
 });
+
+btnEnviarMensagem.addEventListener('click', function () {
+   if (inputNome.value.trim() === '' || inputTexto.value.trim() === '') {
+      alert('Por favor, preencha todos os campos.');
+      return;
+   }
+
+   const novaMensagem = {
+      nome: inputNome.value,
+      mensagem: inputTexto.value
+   };
+
+   let mensagens = JSON.parse(localStorage.getItem('mensagens')) || [];
+   mensagens.push(novaMensagem);
+   localStorage.setItem('mensagens', JSON.stringify(mensagens));
+
+   modalMensagem.style.display = 'none';
+   inputNome.value = '';
+   inputTexto.value = '';
+
+});
+
+
